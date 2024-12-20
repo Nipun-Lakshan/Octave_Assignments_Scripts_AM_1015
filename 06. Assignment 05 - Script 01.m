119cb8faf17072f0ed32e2aa840b74c1093b3f55
Nipun Lakshan <nipunlakshan81@gmail.com>
2024-12-20 22:12:52 +0530

    Script 06

---
+++ 06. Assignment 05 - Script 01.m
@@ -0,0 +1,35 @@
+# Command to Clear The Workspace
+clear all
+
+# Command to Clear The Command Window
+clc
+
+
+### Quetion 01 - Part A
+
+
+# Defining Matrix A
+A = [12 8 18; 8 12 12; 16 8 8]
+
+# Defining Matrix B
+B = [180; 150; 110]
+
+# Finding Unknown Matrix X
+X = inv(A)*B
+
+
+### Quetion 01 - Part b
+
+
+# Quetion d
+
+Cost_of_Producing_One_Box = (1 * 3) + (5 * 2) + (7 * 1) # Rs. 20.00
+
+# Question e
+
+Total_Cost_of_Production = Cost_of_Producing_One_Box * 100 # Rs. 2000.00
+
+# Question f
+
+Weekly_Revenue = 30 * 100 # Rs. 3000.00
+Weekly_Profit = (30 - Cost_of_Producing_One_Box) * 100 # Rs. 1000.00
